e6379e9dcb3e726cc6a43c61e55c8456137f3827
Ericson Luciano <eric@thousandminds.com>
2016-07-14 23:56:40 +0800

    Initial Commit

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    "assets/css/styles.css": "scss/styles.scss"
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: false,
+            },
+            styles: {
+                files: ['scss/**/*.scss'], // which files to watch
+                tasks: ['sass'],
+                options: {
+                    nospawn: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.registerTask('default', ['sass', 'watch']);
+};
